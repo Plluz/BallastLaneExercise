daac58f6f1c42958774f0f86c201917eb8ef4fd9
Pedro Lumertz <plluz@outlook.com>
2023-11-19 18:36:28 -0300

    implement user repository methods with ado.net

--- Assignment.Infrastructure/Repositories/UserRepository.cs
+++ Assignment.Infrastructure/Repositories/UserRepository.cs
@@ -1,22 +1,88 @@
 ﻿using Assignment.Domain.Entities;
 using Assignment.Domain.Interfaces.Repositories;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
 
 namespace Assignment.Infrastructure.Repositories;
 
 public class UserRepository : IUserRepository
 {
+    private readonly string _connectionString;
+
+    public UserRepository(IConfiguration configuration)
+    {
+        _connectionString = (configuration ?? throw new ArgumentNullException(nameof(configuration))).GetConnectionString("AssignmentConnection");
+    }
+
     public async Task<bool> AddAsync(User user)
     {
-        throw new NotImplementedException();
+        using NpgsqlConnection connection = new(_connectionString);
+
+        await connection.OpenAsync();
+
+        string insertQuery = "INSERT INTO Users (Id, Username, Password) VALUES (@id, @username, @password)";
+
+        using NpgsqlCommand command = new(insertQuery, connection);
+
+        command.Parameters.AddWithValue("@id", Guid.NewGuid());
+        command.Parameters.AddWithValue("@username", user.Username);
+        command.Parameters.AddWithValue("@password", user.Password);
+
+        int rowsAffected = await command.ExecuteNonQueryAsync();
+
+        return rowsAffected == 1;
     }
 
     public async Task<User> GetByIdAsync(Guid userId)
     {
-        throw new NotImplementedException();
+        using NpgsqlConnection connection = new(_connectionString);
+
+        await connection.OpenAsync();
+
+        string query = "SELECT * FROM Users WHERE Id = @id";
+
+        using NpgsqlCommand command = new(query, connection);
+
+        command.Parameters.AddWithValue("@id", userId);
+
+        using NpgsqlDataReader reader = await command.ExecuteReaderAsync();
+
+        if (await reader.ReadAsync())
+        {
+            return MapUser(reader);
+        }
+
+        return null;
     }
 
     public async Task<User> GetByUsernameAsync(string username)
     {
-        throw new NotImplementedException();
+        using NpgsqlConnection connection = new(_connectionString);
+
+        await connection.OpenAsync();
+
+        string query = "SELECT * FROM Users WHERE Username = @username";
+
+        using NpgsqlCommand command = new(query, connection);
+
+        command.Parameters.AddWithValue("@username", username);
+
+        using NpgsqlDataReader reader = await command.ExecuteReaderAsync();
+
+        if (await reader.ReadAsync())
+        {
+            return MapUser(reader);
+        }
+
+        return null;
+    }
+
+    private static User MapUser(NpgsqlDataReader reader)
+    {
+        var id = reader.GetGuid(reader.GetOrdinal(nameof(User.Id)));
+        var username = reader.GetString(reader.GetOrdinal(nameof(User.Username)));
+        var password = reader.GetString(reader.GetOrdinal(nameof(User.Password)));
+
+        return new User(id, username, password);
     }
 }
