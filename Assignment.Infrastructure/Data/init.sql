6a52f6b65fb9379cd46011fb667b2b568b33256f
Pedro Lumertz <plluz@outlook.com>
2023-11-19 22:53:00 -0300

    update seed data

--- Assignment.Infrastructure/Data/init.sql
+++ Assignment.Infrastructure/Data/init.sql
@@ -5,6 +5,11 @@ CREATE TABLE IF NOT EXISTS Users (
     Password VARCHAR(64) NOT NULL
 );
 
+-- Insert seed data for Users
+INSERT INTO Users (Id, Username, Password) VALUES
+    (gen_random_uuid (), 'admin', 'password');
+
+
 -- Create Meetings table
 CREATE TABLE IF NOT EXISTS Meetings (
     Id UUID PRIMARY KEY,
@@ -13,12 +18,11 @@ CREATE TABLE IF NOT EXISTS Meetings (
     EndDate TIMESTAMP NOT NULL
 );
 
--- Insert seed data for Users
-INSERT INTO Users (Id, Username, Password) VALUES
-    (gen_random_uuid (), 'admin', 'password');
-
 -- Insert seed data for Meetings
 INSERT INTO Meetings (Id, Title, StartDate, EndDate) VALUES
-    (gen_random_uuid (), 'Test meeting 1', '2023-11-19T13:30:00', '2023-11-19T15:00:00'),
-    (gen_random_uuid (), 'Test meeting 2', '2023-11-19T19:00:00', '2023-11-19T19:15:00'),
-    (gen_random_uuid (), 'Test meeting 3', '2023-11-20T16:00:00', '2023-11-20T17:30:00');
+    (gen_random_uuid (), 'Test meeting 1 (seed)', '2023-11-20T11:00:00', '2023-11-20T11:15:00'),
+    (gen_random_uuid (), 'Test meeting 2 (seed)', '2023-11-19T13:30:00', '2023-11-19T15:00:00'),
+    (gen_random_uuid (), 'Test meeting 3 (seed)', '2023-11-20T16:00:00', '2023-11-20T17:30:00'),
+    (gen_random_uuid (), 'Test meeting 4 (seed)', '2023-11-19T19:00:00', '2023-11-19T19:15:00'),
+    (gen_random_uuid (), 'Test meeting 5 (seed)', '2023-11-20T19:15:00', '2023-11-20T19:30:00'),
+    (gen_random_uuid (), 'Test meeting 6 (seed)', '2023-11-20T19:45:00', '2023-11-20T20:45:00');
